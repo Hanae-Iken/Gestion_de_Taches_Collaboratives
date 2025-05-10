69743176dcd02c3e51f1530844e9ff01e6995307
Hanae-Iken <hanaeiken572@gmail.com>
2025-05-11 00:33:16 +0100

    Fonctionnement de projet

--- src/test/java/ma/ensat/gestion_de_taches_collaboratives/GestionDeTachesCollaborativesApplicationTests.java
+++ src/test/java/ma/ensat/gestion_de_taches_collaboratives/GestionDeTachesCollaborativesApplicationTests.java
@@ -2,8 +2,10 @@ package ma.ensat.gestion_de_taches_collaboratives;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class GestionDeTachesCollaborativesApplicationTests {
 
 	@Test
